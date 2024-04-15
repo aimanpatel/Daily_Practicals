b123a54767b17dd151baea1dcc6d195c83df01c3
aimanpatel <167059315+aimanpatel@users.noreply.github.com>
2024-04-15 11:54:36 +0530

    Add files via upload

---
+++ intro.java
@@ -0,0 +1,10 @@
+public class intro{
+    public static void main(String args[]){
+        System.out.println("My Name is : Aiman Patel..!!");
+        System.out.println("My contact no is : 9421023030");
+        System.out.println("Locations is : Solapur");
+        System.out.println("My Hobbies is reading Books and Travelling...!!!");
+        
+        
+    }
+}
