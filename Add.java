df4c48f0510af88c5c4a772e832212b09aa40e64
aimanpatel <167059315+aimanpatel@users.noreply.github.com>
2024-07-30 09:48:32 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,14 @@
+package com_javapgm;
+
+public class Add {
+
+	public static void main(String[] args) {
+		int num1=3;
+		int num2=8;
+		int sum=num1+num2;
+		System.out.println("sum: "+sum);
+		// TODO Auto-generated method stub
+
+	}
+
+}
