256dc1e50054aa65283bc5b2a037cab9ef18a8d6
aimanpatel <167059315+aimanpatel@users.noreply.github.com>
2024-07-30 09:52:54 +0530

    Add files via upload

---
+++ Pract2.java
@@ -0,0 +1,19 @@
+//For loop Programme...!!!!
+//for Star Triabgle prints
+
+package com_javapgm;
+
+public class Pract2 {
+
+	public static void main(String[] args) {
+		for(int i=1; i<=5; i++)  //Rows conditions
+		{
+			for(int j=1; j<=i; j++)  //Column conditions
+			{
+				System.out.print(" * ");
+			}
+			System.out.println();
+		}
+	}
+
+}
