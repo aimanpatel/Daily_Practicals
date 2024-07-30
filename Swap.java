221b2a5a409d1fc307e16229c8d3f57ef1085b68
aimanpatel <167059315+aimanpatel@users.noreply.github.com>
2024-07-30 09:53:11 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,19 @@
+public class Swap {
+
+	public static void main(String[] args) {
+		int a = 10, b = 20;
+		System.out.println("Before Swap   :");
+		System.out.println("a = "+a);
+		System.out.println("b = "+b);
+		
+		
+		int temp = a;
+		a= b;
+		b = temp;
+		System.out.println("After swap :");
+		System.out.println("a = "+a);
+		System.out.println("b = "+b);
+		
+	}
+
+}
