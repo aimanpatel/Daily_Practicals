f1708b79b5a5a8c99eec5d8e63cbb8183581dd91
aimanpatel <167059315+aimanpatel@users.noreply.github.com>
2024-07-30 09:52:37 +0530

    Add files via upload

---
+++ Prac1.java
@@ -0,0 +1,17 @@
+//While statements.....!!!!
+
+package com_javapgm;
+
+public class Prac1 {
+
+	public static void main(String[] args) {
+		int i=1;
+		while(i<=5)
+		{
+			System.out.println("Hello I am java developer");
+			i++;
+		}
+
+	}
+
+}
